5bb05ed0684716aeb2502f9ab13c78bb3dc4c5c7
Davit Machurishvili <125227660+Davit2605@users.noreply.github.com>
2024-11-04 21:50:02 +0000

    Update data.js

--- data.js
+++ data.js
@@ -4,14 +4,14 @@ export const data = [
     id: "1",
     url: "https://davs-movie-maniac.netlify.app/",
     description: "Movie-maniac",
-    img: "./images/Rotating-nav.png",
+    img: "./images/Screenshot 2024-11-04 214607.png",
   },
  
   {
     id: "2",
     url: "https://davs-react-quizz.netlify.app/",
     description: "React-quiz",
-    img: "./images/Animated-countdown.png",
+    img: ".images/Screenshot 2024-11-04 214631.png",
   },
  
 ];
