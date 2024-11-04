959ac3e220609c01370b440edb2e7287b5e4401f
Davit Machurishvili <125227660+Davit2605@users.noreply.github.com>
2024-11-04 21:29:25 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+
+import { data } from "./data.js";
+
+const array = document.querySelector("#array");
+
+const list = () => {
+  data.map(({ id, description, url, img }) => {
+    const listItem = document.createElement("div");
+    listItem.classList.add("list-item");
+    listItem.style.backgroundImage = `url(${img})`;
+
+    const link = document.createElement("a");
+    link.classList.add("link-item");
+    link.href = `${url}`;
+    link.target = "_blank";
+    link.textContent = "Live Demo";
+
+    const text = document.createElement("p");
+    text.classList.add("text");
+    text.textContent = `${description}`;
+
+    listItem.appendChild(link);
+    listItem.appendChild(text);
+    array.appendChild(listItem);
+  });
+};
+list();
