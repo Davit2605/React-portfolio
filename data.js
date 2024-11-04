a7de0d32f3d80decb7600485ed4879f4caa786b6
Davit Machurishvili <125227660+Davit2605@users.noreply.github.com>
2024-11-04 21:28:32 +0000

    Create data.js

---
+++ data.js
@@ -0,0 +1,249 @@
+export const data = [
+
+    {
+    id: "1",
+    url: "Rotating-nav/index.html",
+    description: "Rotating-nav",
+    img: "./images/Rotating-nav.png",
+  },
+ 
+  {
+    id: "2",
+    url: "Animated-countdown/index.html",
+    description: "Animated-countdown",
+    img: "./images/Animated-countdown.png",
+  },
+  {
+    id: "3",
+    url: "Animated-nav/index.html",
+    description: "Animated-nav",
+    img: "./images/Animated-nav.png",
+  },
+  {
+    id: "4",
+    url: "Background-slider/index.html",
+    description: "Background-slider",
+    img: "./images/Background-slider.png",
+  },
+
+  {
+    id: "5",
+    url: "Vertical-slider/index.html",
+    description: "Vertical-slider",
+    img: "./images/Vertical-slider.png",
+  },
+
+ {
+    id: "6",
+    url: "3D-boxes/index.html",
+    description: "3D-boxes",
+    img: "./images/3D-boxes.png",
+  },
+
+  {
+    id: "7",
+    url: "Clock-project/index.html",
+    description: "Clock-project",
+    img: "./images/Clock-project.png",
+  },
+  
+  {
+    id: "8",
+    url: "Double-heart-click/index.html",
+    description: "Double-heart-click",
+    img: "./images/Double-heart-click.png",
+  },
+  {
+    id: "9",
+    url: "Double-page/index.html",
+    description: "Double-page",
+    img: "./images/Double-page.png",
+  },
+  {
+    id: "10",
+    url: "Drag-drop/index.html",
+    description: "Drag-drop",
+    img: "./images/Drag-drop.png",
+  },
+
+  {
+    id: "11",
+    url: "Tabs-section/index.html",
+    description: "Tabs-section",
+    img: "./images/Tabs-section.png",
+  },
+  {
+    id: "12",
+    url: "Rotating-gallery/index.html",
+    description: "Rotating-gallery",
+    img: "./images/Rotating-gallery.png",
+  },
+
+  {
+    id: "13",
+    url: "Feedback-UI/index.html",
+    description: "Feedback-UI",
+    img: "./images/Feedback-UI.png",
+  },
+  {
+    id: "14",
+    url: "Github-profile/index.html",
+    description: "Github-profile",
+    img: "./images/Github-profile.png",
+  },
+  {
+    id: "15",
+    url: "Image-scroll/index.html",
+    description: "Image-scroll",
+    img: "./images/Image-scroll.png",
+  },
+  {
+    id: "16",
+    url: "Image-search/index.html",
+    description: "Image-search",
+    img: "./images/Image-search.png",
+  },
+  {
+    id: "17",
+    url: "Loading-effect/index.html",
+    description: "Loading-effect",
+    img: "./images/Loading-effect.png",
+  },
+  {
+    id: "18",
+    url: "Movie-UI/index.html",
+    description: "Movie-UI",
+    img: "./images/Movie-UI.png",
+  },
+  {
+    id: "19",
+    url: "Note-app/index.html",
+    description: "Note-app",
+    img: "./images/Note-app.png",
+  },
+  {
+    id: "20",
+    url: "Password-generator/index.html",
+    description: "Password-generator",
+    img: "./images/Password-generator.png",
+  },
+  {
+    id: "21",
+    url: "Password-strength/index.html",
+    description: "Password-strength",
+    img: "./images/Password-strength.png",
+  },
+  {
+    id: "22",
+    url: "Photo-gallery/index.html",
+    description: "Photo-gallery",
+    img: "./images/Photo-gallery.png",
+  },
+  {
+    id: "23",
+    url: "Quiz-app/index.html",
+    description: "Quiz-app",
+    img: "./images/Quiz-app.png",
+  },
+  {
+    id: "24",
+    url: "Random-colors/index.html",
+    description: "Random-colors",
+    img: "./images/Random-colors.png",
+  },
+
+  {
+    id: "25",
+    url: "English-dictionary/index.html",
+    description: "English-dictionary",
+    img: "./images/English-dictionary.png",
+  },
+
+  {
+    id: "26",
+    url: "Check-boxes/index.html",
+    description: "Check-boxes",
+    img: "./images/Check-boxes.png",
+  },
+
+  {
+    id: "27",
+    url: "Scroll-animation/index.html",
+    description: "Scroll-animation",
+    img: "./images/Scroll-animation.png",
+  },
+
+  {
+    id: "28",
+    url: "Drink-water/index.html",
+    description: "Drink-water",
+    img: "./images/Drink-water.png",
+  },
+
+  {
+    id: "29",
+    url: "Testimonial-boxes/index.html",
+    description: "Testimonial-boxes",
+    img: "./images/Testimonial-boxes.png",
+  },
+  {
+    id: "30",
+    url: "Text-animation/index.html",
+    description: "Text-animation",
+    img: "./images/Text-animation.png",
+  },
+
+  {
+    id: "31",
+    url: "Todo-list/index.html",
+    description: "Todo-list",
+    img: "./images/Todo-list.png",
+  },
+
+  {
+    id: "32",
+    url: "Triangle-animation/index.html",
+    description: "Triangle-animation",
+    img: "./images/Triangle-animation.png",
+  },
+  {
+    id: "33",
+    url: "User-listUI/index.html",
+    description: "User-listUI",
+    img: "./images/User-listUI.png",
+  },
+  {
+    id: "34",
+    url: "Verify-account/index.html",
+    description: "Verify-account",
+    img: "./images/Verify-account.png",
+  },
+
+  {
+    id: "35",
+    url: "Basic-calculator/index.html",
+    description: "Basic-calculator",
+    img: "./images/Basic-calculator.png",
+  },
+
+  {
+    id: "36",
+    url: "Todos/index.html",
+    description: "Todos",
+    img: "./images/Todos.png",
+  },
+
+  {
+    id: "37",
+    url: "Video-trailer/index.html",
+    description: "Video-trailer",
+    img: "./images/Video-trailer.png",
+  },
+
+  {
+    id: "38",
+    url: "Website-nav/index.html",
+    description: "Website-nav",
+    img: "./images/Website-nav.png",
+  },
+];
