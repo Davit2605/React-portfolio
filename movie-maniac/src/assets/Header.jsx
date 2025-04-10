27737861fcc5d2269f4e861ec8ded904fd7f916b
Davit Machurishvili <125227660+Davit2605@users.noreply.github.com>
2025-04-10 21:05:05 +0100

    Update Header.jsx

--- movie-maniac/src/assets/Header.jsx
+++ movie-maniac/src/assets/Header.jsx
@@ -4,6 +4,19 @@ const Header = () => {
   return (
     <nav className=" align-items nav-container ">
       <h2 className="header">🎞️ Free Movies 🎞️</h2>
+        <div className="header-links">
+        <ul className="align-items">
+          <li>
+            <a>Popular 🔥</a>
+          </li>
+          <li>
+            <a >Top-Rated ⭐</a>
+          </li>
+          <li>
+            <a>Upcoming 🥳</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
