aad916b841e141ffec5a7a52b9eb4f0d789150c7
Davit Machurishvili <125227660+Davit2605@users.noreply.github.com>
2025-04-10 23:26:23 +0100

    Add files via upload

---
+++ Movie-maniac/src/assets/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <nav className=" align-items nav-container ">
+      <h2 className="header">🎞️ Free Movies 🎞️</h2>
+    </nav>
+  );
+};
+
+export default Header;
